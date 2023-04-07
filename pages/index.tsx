352cf4f79f5e1a9ee7ef56db37cb3596ddad70b5
sigani <82123942+sigani@users.noreply.github.com>
2023-04-07 11:42:37 -0700

    fux

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import Head from 'next/head'
-import clientPromise from '../lib/mongodb'
-import { InferGetServerSidePropsType } from 'next'
+import Head from "next/head";
+import clientPromise from "../lib/mongodb";
+import { InferGetServerSidePropsType } from "next";
 
-export async function getServerSideProps(context) {
+export async function getServerSideProps(context: any) {
   try {
-    await clientPromise
+    await clientPromise;
     // `await clientPromise` will use the default database passed in the MONGODB_URI
     // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
     //
@@ -16,12 +16,12 @@ export async function getServerSideProps(context) {
 
     return {
       props: { isConnected: true },
-    }
+    };
   } catch (e) {
-    console.error(e)
+    console.error(e);
     return {
       props: { isConnected: false },
-    }
+    };
   }
 }
 
@@ -44,7 +44,7 @@ export default function Home({
           <h2 className="subtitle">You are connected to MongoDB</h2>
         ) : (
           <h2 className="subtitle">
-            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
+            You are NOT connected to MongoDB. Check the <code>README.md</code>{" "}
             for instructions.
           </h2>
         )}
@@ -92,7 +92,7 @@ export default function Home({
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
@@ -247,5 +247,5 @@ export default function Home({
         }
       `}</style>
     </div>
-  )
+  );
 }
