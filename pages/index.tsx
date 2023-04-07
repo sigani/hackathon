afcfb1b483f7b7355cffcbc89cf900f8cd13745a
sigani <82123942+sigani@users.noreply.github.com>
2023-04-07 14:46:40 -0700

    fix maybe

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,31 +5,29 @@ import { Container, Grid, Stack } from "@mui/material";
 import TopBar from "./components/TopBar";
 import SearchJobs from "./components/SearchJobs";
 import Jobs from "./components/Jobs";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
-export async function getServerSideProps(context: any) {
-  try {
-    await clientPromise;
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
+// export async function getServerSideProps(context: any) {
+//   try {
+//     await clientPromise;
+//     // `await clientPromise` will use the default database passed in the MONGODB_URI
+//     // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+//     //
+//     // `const client = await clientPromise`
+//     // `const db = client.db("myDatabase")`
+//     //
+//     // Then you can execute queries against your database like so:
+//     // db.find({}) or any of the MongoDB Node Driver commands
 
-    return {
-      props: { isConnected: true },
-    };
-  } catch (e) {
-    console.error(e);
-    return {
-      props: { isConnected: false },
-    };
-  }
-}
+//     return {
+//       props: { isConnected: true },
+//     };
+//   } catch (e) {
+//     console.error(e);
+//     return {
+//       props: { isConnected: false },
+//     };
+//   }
+// }
 
 export default function Home({
   isConnected,
