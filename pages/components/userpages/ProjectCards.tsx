ab55d8971ee79a92b43c01ad99f4c04f11f77f3d
sigani <82123942+sigani@users.noreply.github.com>
2023-04-08 18:30:05 -0700

    added default project prop

--- pages/components/userpages/ProjectCards.tsx
+++ pages/components/userpages/ProjectCards.tsx
@@ -16,12 +16,29 @@ import {
   Grid,
   IconButton,
 } from "@mui/material";
+import { Project } from "../../../interfaces/Project";
 
 let pastproject = false;
 let title = "BlackJack";
 let description =
   "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
 let projectIm = "/projects/p1.png";
+
+let dummyProject: Project = {
+  id: "69",
+  name: title,
+  type: "Video Game",
+  languages: ["JavaScript"],
+  description: description,
+  owner: "69",
+  members: ["69"],
+  teamSize: 2,
+  openForApplications: true,
+  completed: true,
+  photos: [],
+  likes: [],
+  website: "",
+};
 
 function ProjectSummary({ handleOpen, project }: any) {
   return (
@@ -48,7 +65,7 @@ function ProjectSummary({ handleOpen, project }: any) {
   );
 }
 
-export default function ProjectCards({ project }: any) {
+export default function ProjectCards({ project = dummyProject }: any) {
   console.log(project);
   // would first make api call to get all available projects
   const [open, setOpen] = useState(false);
